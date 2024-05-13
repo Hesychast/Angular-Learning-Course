8e31ef800586e9bb1e5f4049e6f6d4bbdf99b2e4
Volodymyr <wdvlpr@gmail.com>
2024-05-13 17:05:14 +0300

    The Empolyes componetn was generated.

--- LearnComponents/src/app/app.module.ts
+++ LearnComponents/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployesComponent } from './Components/employes/employes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployesComponent
   ],
   imports: [
     BrowserModule
