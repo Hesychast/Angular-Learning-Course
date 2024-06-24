a368ed14dcb1076a4ab2e4eb65b5b6f5e6833966
Volodymyr <wdvlpr@gmail.com>
2024-06-24 14:02:02 +0300

    Optimization.

--- articles-app/src/app/app.route.ts
+++ articles-app/src/app/app.route.ts
@@ -1,15 +1,11 @@
 import { Routes } from '@angular/router';
-import { ArticleListComponent } from './components/article-list/article-list.component';
-import { ArticleDetailComponent } from './components/article-detail/article-detail.component';
-import { ArticleFormComponent } from './components/article-form/article-form.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/articles', pathMatch: 'full' },
-  { path: 'articles', component: ArticleListComponent, pathMatch: 'full' },
-  { path: 'articles/:id', component: ArticleDetailComponent },
-  { path: 'edit-article/:id', component: ArticleFormComponent },
-  { path: 'add-article', component: ArticleFormComponent },
+  { path: 'articles', loadComponent: () => import('./components/article-list/article-list.component').then(m => m.ArticleListComponent), pathMatch: 'full' },
+  { path: 'articles/:id', loadComponent: () => import('./components/article-detail/article-detail.component').then(m => m.ArticleDetailComponent) },
+  { path: 'edit-article/:id', loadComponent: () => import('./components/article-form/article-form.component').then(m => m.ArticleFormComponent) },
+  { path: 'add-article', loadComponent: () => import('./components/article-form/article-form.component').then(m => m.ArticleFormComponent) },
   { path: '404/:id', loadComponent: () => import('./components/not-found/not-found.component').then(m => m.NotFoundComponent) },
   { path: '**', redirectTo: '/404' }
 ];
--- articles-app/src/app/components/article-form/article-form.component.ts
+++ articles-app/src/app/components/article-form/article-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ArticleStorageService } from '../../services/article-storage.service';
 import { ArticleStateService } from '../../services/article-state-service.service';
